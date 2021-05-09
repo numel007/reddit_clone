08f2e2dfad4ee17d09055333f15f42fb1d69221d
Jackie <taekwondo.chan@gmail.com>
2021-05-09 12:35:18 -0700

    add voting

--- server.js
+++ server.js
@@ -39,6 +39,7 @@ require("./controllers/replies.js")(app);
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+app.use(express.static("public"));
 
 // Routes
 app.get("/", (req, res) => {
